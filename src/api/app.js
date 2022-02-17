dd5c24fb37ddbddf3629b43cbfd4dbb7b47a4510
Renata Nunes <82226758+renatapnunes@users.noreply.github.com>
2022-02-16 23:15:27 -0300

    Configured CORS (#4)

--- src/api/app.js
+++ src/api/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
 const router = require('../routes');
 const error = require('../middlewares/error');
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['POST', 'GET', 'PUT', 'DELETE'],
+}));
 
 app.use(express.json());
 app.use(router);
